67ca777345caeff8afdf217fe15069e4a7a891e7
Kanmii <maneptha@gmail.com>
2020-06-20 09:56:25 +0200

    initial commit: typescript works in script tag but not imported .ts file

---
+++ src/routes/_utils.index.ts
@@ -0,0 +1,1 @@
+export let title = "Hallo Leute";
---
+++ src/routes/index.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  /* Could not resolve './_utils.index' from src/routes/index.svelte */
+  /* import {title} from "./_utils.index"; */
+
+  export let title: string = "Hallo Ts";
+
+  interface Some {
+    a: 1;
+    b: "two";
+    c: string;
+    d: Date;
+  }
+
+  const some: Some = {
+    a: 1,
+    b: "two",
+    c: "three",
+    d: new Date()
+  };
+</script>
+
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<figure>
+  <img alt="Success Kid" src="successkid.jpg" />
+  <figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p>
+  <strong>
+    Try editing this file (src/routes/index.svelte) to test live reloading.
+  </strong>
+</p>
